5d1274c1d0ac1642309893e29f60be79e3d063dd
Chris Wilkinson <zubizan@gmail.com>
2014-12-11 20:50:41 -0500

    WIZARD WITH RANDOM NAMES

--- mobile/src/main/java/com/benhgreen/wizards/MainActivity.java
+++ mobile/src/main/java/com/benhgreen/wizards/MainActivity.java
@@ -7,10 +7,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
+String[] wizardarray1= {"Wizard","Warlock","Mage","Sage","Manny","Manuel","Manfred"};
+    String[] wizardarray2= {"Power","Magic","Warlocks","Idiots","Manny","Manuel","Manfred"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +24,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         ImageButton image = (ImageButton) findViewById(R.id.wizardView2);
         image.setVisibility (View.INVISIBLE);
-            }
+                }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,11 +55,20 @@ public class MainActivity extends ActionBarActivity {
         //toast.show();
      }
     public void buttonclick(View view){
+        Random rand =new Random ();
+        int x = rand.nextInt(wizardarray1.length);
+        String wizardvariable = wizardarray1[x];
+        x = rand.nextInt(wizardarray2.length);
+        String wizardvariable2 = wizardarray2[x];
         ImageButton image = (ImageButton) findViewById(R.id.wizardView1);
         ImageButton img = (ImageButton) findViewById(R.id.wizardView2);
-                img.bringToFront();
+        TextView text = (TextView) findViewById(R.id.textView);
+                               img.bringToFront();
         image.setVisibility (View.INVISIBLE);
         img.setVisibility (View.VISIBLE);
+        text.setVisibility (View.VISIBLE);
+        text.setText(wizardvariable + " of " + wizardvariable2);
+
        //Toast toast = Toast.makeText(getApplicationContext(), "Button 1 Pressed", Toast.LENGTH_SHORT);
        //toast.show();
 
