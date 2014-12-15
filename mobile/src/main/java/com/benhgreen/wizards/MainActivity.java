7324f2ca8f40d1bd96d972cc20082e841e8b1a7b
Chris Wilkinson <zubizan@gmail.com>
2014-12-15 03:28:03 -0500

    Clean Wizards

--- mobile/src/main/java/com/benhgreen/wizards/MainActivity.java
+++ mobile/src/main/java/com/benhgreen/wizards/MainActivity.java
@@ -1,75 +1,51 @@
 package com.benhgreen.wizards;
-
 import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import java.util.Random;
 
-
 public class MainActivity extends ActionBarActivity {
-String[] wizardarray1= {"Wizard","Warlock","Mage","Sage","Manny","Manuel","Manfred"};
+    String[] wizardarray1= {"Wizard","Warlock","Mage","Sage","Manny","Manuel","Manfred"};
     String[] wizardarray2= {"Power","Magic","Warlocks","Idiots","Manny","Manuel","Manfred"};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton image = (ImageButton) findViewById(R.id.wizardView2);
-        image.setVisibility (View.INVISIBLE);
                 }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    public void buttonclick2(View view){
-        ImageButton img = (ImageButton) findViewById(R.id.wizardView1);
-        ImageButton image = (ImageButton) findViewById(R.id.wizardView2);
-        //Toast toast = Toast.makeText(getApplicationContext(), "Button 2 Pressed", Toast.LENGTH_SHORT);
-        //toast.show();
-     }
-    public void buttonclick(View view){
+    public void generatewizard (View view){
         Random rand =new Random ();
-        int x = rand.nextInt(wizardarray1.length);
-        String wizardvariable = wizardarray1[x];
-        x = rand.nextInt(wizardarray2.length);
-        String wizardvariable2 = wizardarray2[x];
         ImageButton image = (ImageButton) findViewById(R.id.wizardView1);
-        ImageButton img = (ImageButton) findViewById(R.id.wizardView2);
+        ImageView img = (ImageView) findViewById(R.id.wizardView2);
         TextView text = (TextView) findViewById(R.id.textView);
-                               img.bringToFront();
+        int x = rand.nextInt(wizardarray1.length);
+            String wizardvariable = wizardarray1[x];
+             x = rand.nextInt(wizardarray2.length);
+              String wizardvariable2 = wizardarray2[x];
+                text.setText(wizardvariable + " of " + wizardvariable2);
+
+        img.bringToFront();
         image.setVisibility (View.INVISIBLE);
         img.setVisibility (View.VISIBLE);
         text.setVisibility (View.VISIBLE);
-        text.setText(wizardvariable + " of " + wizardvariable2);
 
-       //Toast toast = Toast.makeText(getApplicationContext(), "Button 1 Pressed", Toast.LENGTH_SHORT);
-       //toast.show();
-
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.hoooo);
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.hoooo);
                      mp.start();
         mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                ImageButton image = (ImageButton) findViewById(R.id.wizardView1);
-                ImageButton img = (ImageButton) findViewById(R.id.wizardView2);
-                image.bringToFront();
-                img.setVisibility (View.INVISIBLE);
-                image.setVisibility (View.VISIBLE);
-                mp.release();
-            }
+                public void onCompletion(MediaPlayer mp) {
+                    ImageButton image = (ImageButton) findViewById(R.id.wizardView1);
+                      ImageView img = (ImageView) findViewById(R.id.wizardView2);
+                       image.bringToFront();
+                     img.setVisibility (View.INVISIBLE);
+                       image.setVisibility (View.VISIBLE);
+                     mp.release();
+                            }
         });
 
     }
